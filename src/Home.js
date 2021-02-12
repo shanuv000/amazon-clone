bd93764a4a0c771b54dc07539979a79cc8ddffc1
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-02-13 00:23:22 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import Product from "./Product";
 
 function Home() {
-    return <div className='home'>
+    return (<div className='home'>
         <div className="home__container">
             <img className='home_image'
                  src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/Feb/HI_EN/1500x600_Hero-Tall_NP._CB660043011_.jpg"
                  alt=""/>
 
             <div className="home__row">
-                {/*Product*/}{/*Product*/}
+                <Product/><Product/>
+                {/*Product*/}
             </div>
             <div className="home__row">
-                {/*Product*/}{/*Product*/}{/*Product*/}
+                <Product/><Product/><Product/>
             </div>
             <div className="home__row">
                 {/*Product*/}
@@ -20,7 +22,7 @@ function Home() {
 
 
         </div>
-    </div>
+    </div>)
 }
 
 export default Home;
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,28 @@
 import React from 'react';
-function product()
-{
- return ()
+import './Product.css';
+
+function Product() {
+    return (
+        <div className='product'>
+            <div className="product__info">
+                <p>The lean Startup</p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <strong>199.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>🌟</p>
+                    <p>🌟</p>
+                    <p>🌟</p>
+                </div>
+            </div>
+            <img
+                src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Grocery/GW/Chocolates_PC_CC_379x304_V2._SY304_CB404777310_.jpg"
+                alt=""/>
+            <button>Add to Basket</button>
+        </div>
+    )
 
 }
-export default product;
+
+export default Product;
