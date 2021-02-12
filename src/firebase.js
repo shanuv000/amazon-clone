24928215084e145b78e7e4922d20c43d1dcd893e
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-02-12 21:13:21 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAjo1tp2H9JDXGZZFBscW9v8eQmxSRuEVk",
+    authDomain: "challenge-31446.firebaseapp.com",
+    projectId: "challenge-31446",
+    storageBucket: "challenge-31446.appspot.com",
+    messagingSenderId: "203518345781",
+    appId: "1:203518345781:web:744c4de5f3a094ee5ea77c",
+    measurementId: "G-4F87850TP5"
+};
