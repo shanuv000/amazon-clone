f09e6e8d2fc216a844a714fdf55aaf831a2b943d
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-02-12 23:37:48 +0530

    commit

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Home.css'
+
+function Home() {
+    return <div className='home'>
+        <div className="home__container">
+            <img className='home_image'
+                 src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2021/X-site/Multititle/Feb/HI_EN/1500x600_Hero-Tall_NP._CB660043011_.jpg"
+                 alt=""/>
+
+            <div className="home__row">
+                {/*Product*/}{/*Product*/}
+            </div>
+            <div className="home__row">
+                {/*Product*/}{/*Product*/}{/*Product*/}
+            </div>
+            <div className="home__row">
+                {/*Product*/}
+            </div>
+
+
+        </div>
+    </div>
+}
+
+export default Home;
