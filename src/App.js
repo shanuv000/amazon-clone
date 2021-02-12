7ae30da951e66fbb10e660f8e6ae6ed72c205725
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2021-02-12 21:13:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./Header";
+
+function App() {
+    return (
+        <div className="app">
+            <Header/>
+            {/*{Home}*/}
+        </div>
+    );
+}
+
+export default App;
